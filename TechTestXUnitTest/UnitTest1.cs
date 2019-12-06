6057a5540a3640474268d1c2a5006de76579535b
heitor <heitor@GLS-DEV>
2019-12-06 14:52:08 -0300

    added ShouldProcessBeValidTest

--- TechTestXUnitTest/UnitTest1.cs
+++ TechTestXUnitTest/UnitTest1.cs
@@ -1,16 +1,28 @@
-using System;
 using Xunit;
 using TechTest;
+using FluentAssertions;
 
 namespace TechTestXUnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("IRELAND", "1", "1")]
+        [InlineData("IRELAND", "1", "2")]
+        [InlineData("germany", "10", "20")]
+        public void ShouldProcessBeValidTest(string employeelocation, string srthoursworked, string srthourlyrate)
         {
+            // arrange
             var consoleContext = new ConsoleContext();
-            consoleContext.Test();
+            var result = string.Empty;
+
+            // act
+            consoleContext.Init(new UserInput(srthoursworked, srthourlyrate,employeelocation));
+            consoleContext.Process();
+            result = consoleContext.OutPutResult();
+
+            // assert
+            result.Should().NotBeNullOrWhiteSpace();
         }
     }
 }
